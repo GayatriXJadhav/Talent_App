a3bb70d74b355e81a8d3c364da2f64e18e3d1a6a
GayatriXJadhav <gayatrijadhav1202@gmail.com>
2025-11-13 17:41:05 +0530

    backend fixed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 const cors=require("cors");
-const path = require("path");
+
 const talentroutes=require("./routes/talentroute");
 
 dotenv.config();
@@ -14,14 +14,7 @@ app.use(cors());
 app.use(express.json());
 //routes
 app.use("/api",talentroutes);
-if (process.env.NODE_ENV === "production") {
-  const __dirname1 = path.resolve();
-  app.use(express.static(path.join(__dirname1, "/client/dist")));
 
-  app.use((req, res, next) => {
-    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
-  });
-}
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Db connected"))
 .catch(err=>console.error("Db connection failed:",err));
