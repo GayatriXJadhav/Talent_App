3e091e44f4f10de3e3beb33b28f45176575bd1a1
GayatriXJadhav <gayatrijadhav1202@gmail.com>
2025-11-13 14:44:33 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,25 @@ const talentroutes=require("./routes/talentroute");
 dotenv.config();
 
 const app=express();
+
 //middleware
 app.use(cors());
 app.use(express.json());
 //routes
 app.use("/api",talentroutes);
+if (process.env.NODE_ENV === "production") {
 
+
+  app.use(express.static(path.join(__dirname, "../client/dist"))); 
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+  });
+}
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Db connected"))
 .catch(err=>console.error("Db connection failed:",err));
+
 
 const PORT=process.env.port || 5000;
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
